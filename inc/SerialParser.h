b92540d8ea86709e57859d4652cef84681c2331a
Daniele Ferro <daniele.ferro86@gmail.com>
2015-02-02 12:28:13 +0100

    SerialPaser header and arduino code

---
+++ inc/SerialParser.h
@@ -0,0 +1,42 @@
+#ifndef __SERIAL_PARSER_HEADER
+#define __SERIAL_PARSER_HEADER
+
+#define BUF_MAX_LENGTH                   64
+#define CMD_BEGIN_IND                    94 // ^
+#define CMD_END_IND                      10 // Line Feed (LF)
+
+#ifdef ARDUINO
+#include <Arduino.h>
+#else
+typedef unsigned char       byte;
+#endif
+
+
+class SerialParser
+{
+
+public:
+    SerialParser(void);
+    SerialParser(byte cmdBeginIndicator, byte cmdEndIndicator);
+
+    void (*ReceiverPtr)(byte* buf, byte cmdLength);
+    void (*SenderPtr)(byte buf);
+
+    void Receive(byte data);
+    void Send(byte cmd);
+    void Send(byte* cmd, byte cmdLength);
+
+private:
+	
+    byte cmdBufferIdx;
+    byte buf[BUF_MAX_LENGTH];
+    bool cmdBegan;
+    bool bufferFull;
+
+    byte bufMaxLength;
+    byte cmdBeginInd;
+    byte cmdEndInd;
+
+};
+
+#endif // __SERIAL_PARSER_HEADER
