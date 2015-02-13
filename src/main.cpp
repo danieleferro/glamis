830ce7bea935f839c2df9233624b10d6bd4e7fa5
Daniele Ferro <daniele.ferro86@gmail.com>
2015-02-13 08:47:06 +0100

    wifi, WIP

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,7 @@ void setup(void)
 			  WIFI_CHANNEL, ENCR_WAP_WAP2_PSK);
     */
     res = wifi.confMode(ESP8266_MODE_STA);
+    wifi.Reset();
     delay(10000);
     res = wifi.showAP(buffer, BUFFER_SIZE);
 
