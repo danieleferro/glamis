82b33a97061525f0b6fe1ded3b35f13295ffecf8
Daniele Ferro <daniele.ferro86@gmail.com>
2015-09-18 19:07:01 +0200

    scripts fix

--- third/scripts/glamis_at_off.sh
+++ third/scripts/glamis_at_off.sh
@@ -4,6 +4,6 @@
 # off at 19:31 on the same day
 
 at -M 19:31 2>/tmp/glamis_off_id <<EOF
-/home/daniele/glamis/glamis_server -c off
+~/glamis/glamis_server -c off
 EOF
 
--- third/scripts/glamis_at_on.sh
+++ third/scripts/glamis_at_on.sh
@@ -4,6 +4,6 @@
 # on at 19:01 on the same day
 
 at -M 19:01 2>/tmp/glamis_on_id <<EOF
-/home/daniele/glamis/glamis_server -c on
+~/glamis/glamis_server -c on
 EOF
 
